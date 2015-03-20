fd58d3d6ff2f378fe2225768d5001c910ac41ca8
Cyril Fluck <github@fluck.fr>
2015-03-20 15:49:28 -0700

    The config panel has access to the widget

--- app/views/config.js
+++ app/views/config.js
@@ -4,6 +4,8 @@ export default Ember.View.extend({
   configPanel: function() {
     var componentName = this.get('controller.configPanel');
 
-    return this.container.lookupFactory('component:' + componentName).create();
+    return this.container.lookupFactory('component:' + componentName).create({
+      widget: this.get('controller.model')
+    });
   }.property('controller.configPanel')
 });
